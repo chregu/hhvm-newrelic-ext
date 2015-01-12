0ed7810f2d784cd2be8cce25c889c12da1c9d294
Christian Stocker <me@chregu.tv>
2015-01-12 20:29:22 +0100

    include hphp-system for version

--- newrelic.cpp
+++ newrelic.cpp
@@ -1,6 +1,7 @@
 #include "hphp/runtime/base/base-includes.h"
 #include "hphp/runtime/ext/std/ext_std_errorfunc.h"
 #include "hphp/runtime/base/php-globals.h"
+#include "hphp/runtime/base/hphp-system.h"
 #include "hphp/runtime/ext/ext_hotprofiler.h"
 #include "newrelic_transaction.h"
 #include "newrelic_collector_client.h"
